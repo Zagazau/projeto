68d0c33d19fdf1c198f8549eb44b52ae501081f4
tiagop23 <talexandrea@ipvc.pt>
2024-05-05 17:05:49 +0100

    controller e xmls

---
+++ desktop/src/main/java/com/example/desktop/MenuClienteController.java
@@ -0,0 +1,30 @@
+package com.example.desktop;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MenuClienteController {
+
+    @FXML
+    private Button consultarProdutosButton;
+
+    @FXML
+    private Button goBack;
+
+    @FXML
+    private Button pagamentosButton;
+
+    @FXML
+    private Button pedidosButton;
+
+    @FXML
+    private Button sairButton;
+
+
+}
